05925f1e253b9ea08ae140a8c53a5db62a169bbe
Donald <ediagee@yahoo.com>
2022-08-11 20:05:52 +0300

    added deployments in travis

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <Link to={"/"}>Home</Link>
+          <Link to={"/"}>Home bubb</Link>
           <Link to={"/otherpage"}>Other Page !!</Link>
           <div>
             <Route exact path={"/"} component={Fib} />
